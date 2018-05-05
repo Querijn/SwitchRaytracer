7ddcaba41336247e9fad9c74b745cc51368b0120
Querijn Heijmans <querijn@irule.at>
2018-05-05 23:17:32 +0200

    Clean up GetColour function

--- Raytracer/BlinnPhongMaterial.cpp
+++ Raytracer/BlinnPhongMaterial.cpp
@@ -19,19 +19,17 @@ BlinnPhongMaterial::~BlinnPhongMaterial()
 glm::vec3 BlinnPhongMaterial::GetColour(const Ray& a_Ray, const Primitive* a_Primitive, float a_Distance) const
 {
 	glm::vec3 t_Position = a_Primitive->GetPosition();
-	glm::vec3 t_LightPosition = /*t_Position +*/ glm::vec3(0, 15, 0);
+	glm::vec3 t_LightPosition = glm::vec3(0, 15, 0);
 	glm::vec3 t_LightDir = glm::normalize(t_LightPosition - t_Position);
 	glm::vec3 t_RayDir = -a_Ray.Direction;
 	glm::vec3 t_HalfWay = glm::normalize(t_LightDir + t_RayDir);
 	glm::vec3 t_Normal = a_Primitive->GetNormal(a_Ray, a_Distance);
 
-	glm::vec3 ambient = m_Colour * 0.05f;
-	glm::vec3 normal = glm::normalize(t_Normal);
-	float diff = max(dot(t_LightDir, normal), 0.0);
-	glm::vec3 diffuse = diff * m_Colour;
+	glm::vec3 t_Ambient = m_Colour * 0.05f;
+	glm::vec3 t_Diffuse = m_Colour * max(dot(t_LightDir, t_Normal), 0.0f);
 
-	glm::vec3 t_LightBounce = glm::reflect(-t_LightDir, normal);
+	glm::vec3 t_LightBounce = glm::reflect(-t_LightDir, t_Normal);
 	float spec = pow(max(dot(t_RayDir, t_LightBounce), 0.0), m_SpecularHardness) * m_SpecularStrength;
 
-	return glm::vec3(ambient + diffuse + spec);
+	return glm::vec3(t_Ambient + t_Diffuse + spec);
 }
