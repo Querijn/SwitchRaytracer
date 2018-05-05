e657331ed2ff9ba854c062102e1cc578e2b49db2
Querijn Heijmans <querijn@irule.at>
2018-05-05 23:15:16 +0200

    Make specular option variable

--- Raytracer/BlinnPhongMaterial.cpp
+++ Raytracer/BlinnPhongMaterial.cpp
@@ -5,8 +5,8 @@
 #include "Ray.h"
 #include "Primitive.h"
 
-BlinnPhongMaterial::BlinnPhongMaterial(const glm::vec3& a_Colour) :
-	m_Colour(a_Colour)
+BlinnPhongMaterial::BlinnPhongMaterial(const glm::vec3& a_Colour, float a_SpecularHardness, float a_SpecularStrength) :
+	m_Colour(a_Colour), m_SpecularHardness(a_SpecularHardness), m_SpecularStrength(a_SpecularStrength)
 {
 
 }
@@ -31,7 +31,7 @@ glm::vec3 BlinnPhongMaterial::GetColour(const Ray& a_Ray, const Primitive* a_Pri
 	glm::vec3 diffuse = diff * m_Colour;
 
 	glm::vec3 t_LightBounce = glm::reflect(-t_LightDir, normal);
-	float spec = pow(max(dot(t_RayDir, t_LightBounce), 0.0), 10.0) * 0.5;
+	float spec = pow(max(dot(t_RayDir, t_LightBounce), 0.0), m_SpecularHardness) * m_SpecularStrength;
 
 	return glm::vec3(ambient + diffuse + spec);
 }
--- Raytracer/BlinnPhongMaterial.h
+++ Raytracer/BlinnPhongMaterial.h
@@ -5,12 +5,13 @@
 class BlinnPhongMaterial : public BaseMaterial
 {
 public:
-	BlinnPhongMaterial(const glm::vec3& a_Colour);
+	BlinnPhongMaterial(const glm::vec3& a_Colour, float a_SpecularHardness, float a_SpecularStrength);
 	~BlinnPhongMaterial();
 
 	glm::vec3 GetColour(const Ray& a_Ray, const Primitive* a_Primitive, float a_Distance) const override;
 
 private:
 	glm::vec3 m_Colour;
+	float m_SpecularHardness, m_SpecularStrength;
 };
 
